bc9f93b45d2e543df38894c03cc8f3cd207978c4
Clare Jackson <clare-jackson@lambdastudents.com>
2020-08-20 22:26:25 -0400

    Warm up questions complete.

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
+let votingAge = 38;
 
-
+if(votingAge > 18) {
+  console.log(true);
+} else {
+  console.log(false);
+}
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
+let numOne = 2;
+let numTwo = 4;
 
-
+if(numTwo < numOne) {
+  numOne = numOne + 1
+  console.log('numOne is bigger', numOne)
+} else {
+  console.log('numOne is smaller', numOne)
+}
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
 
-
+ console.log(parseInt('1999'))
 
 
 //Task d: Write a function to multiply a*b 
 
+function multiply(a, b){
+  return a * b;
+};
 
-
-
+console.log(multiply(4,7));
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
-
-
+function
+myAge * 7
 
 
 
